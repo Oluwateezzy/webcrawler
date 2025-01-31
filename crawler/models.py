929c7656e6e79cc79e483e1fa03a7623ceab96b4
Oluwatobiloba <oluwateezzy03@gmail.com>
2025-01-31 01:55:03 +0100

    add CrawledPage model and initial migration for webcrawler project

--- crawler/models.py
+++ crawler/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class CrawledPage(models.Model):
+    url = models.URLField(unique=True)
+    title = models.CharField(max_length=255, blank=True, null=True)
+    content = models.TextField(blank=True, null=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.url
