480846be277755f2a4df4f114ff819937576d984
Oluwatobiloba <oluwateezzy03@gmail.com>
2025-02-06 12:43:07 +0100

    update start_crawl view to print request data and modify response; add .gitignore for environment files

--- crawler/views.py
+++ crawler/views.py
@@ -7,8 +7,9 @@ from crawler.tasks import crawl_task
 
 
 def start_crawl(request):
-    url = request.Get().get("url", "")
-    if not url:
-        return JsonResponse({"error": "URL parameter is required"}, status=400)
-    crawl_task.delay(url)
-    return JsonResponse({"message": "Crawling started", "url": url})
+    print(request.get)
+    # url = request.Get.get("url", "")
+    # if not url:
+    #     return JsonResponse({"error": "URL parameter is required"}, status=400)
+    # crawl_task.delay(url)
+    return JsonResponse({"message": "Crawling started", "url": "url"})
